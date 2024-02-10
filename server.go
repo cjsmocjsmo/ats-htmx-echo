5f90058fdd129bb65dc8ddab740bdc098eecc979
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-02-10 02:12:33 -0800

    Fix database connection issue

--- server.go
+++ server.go
@@ -27,7 +27,8 @@ type Template struct {
 func init() {
 	godotenv.Load()
 	dbpath := os.Getenv("ATS_DB_PATH")
-	db, err := sql.Open("sqlite3", dbpath)
+	db, err := sql.Open("sqlite3", ats.db)
+
 	if err != nil {
 		panic(err)
 	}
